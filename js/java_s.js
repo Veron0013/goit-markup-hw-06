3a00e18a6bf21691190c18467e3b25038b9ca351
Mr.Igor <iv_mirsoft@ukr.net>
2025-03-06 21:35:35 +0200

    finish

--- js/java_s.js
+++ js/java_s.js
@@ -1,15 +1,20 @@
 
 var btnOpen = document.getElementById("open_form");
-var btnOpenMenu = document.getElementById("mobile_menu");
 var btnClose = document.getElementById("close_form");
 var myModalForm = document.getElementById("modal_form");
+
+var btnOpenMenu = document.getElementById("open_menu");
+var btnCloseMenu = document.getElementById("close_menu");
 var myModalMenu = document.getElementById("modal_menu");
 
-function toggleClass() {
-	myModalMenu.classList.toggle("is-open");
-	myModalForm.classList.toggle("is-open");
+function toggleClass(objForm) {
+	return function () {
+		objForm.classList.toggle("is-open");
+	};
 }
 
-btnOpen.addEventListener("click", toggleClass);
-btnOpenMenu.addEventListener("click", toggleClass);
-btnClose.addEventListener("click", toggleClass);
+btnOpen.addEventListener("click", toggleClass(myModalForm));
+btnClose.addEventListener("click", toggleClass(myModalForm));
+
+btnOpenMenu.addEventListener("click", toggleClass(myModalMenu));
+btnCloseMenu.addEventListener("click", toggleClass(myModalMenu));
