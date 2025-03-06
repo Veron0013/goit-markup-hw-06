55915bd27925ed22958709e8c00fa24c809c7254
Mr.Igor <iv_mirsoft@ukr.net>
2025-03-06 10:08:55 +0200

    keypoints

--- js/java_s.js
+++ js/java_s.js
@@ -1,11 +1,15 @@
 
 var btnOpen = document.getElementById("open_form");
+var btnOpenMenu = document.getElementById("mobile_menu");
 var btnClose = document.getElementById("close_form");
 var myModalForm = document.getElementById("modal_form");
+var myModalMenu = document.getElementById("modal_menu");
 
 function toggleClass() {
+	myModalMenu.classList.toggle("is-open");
 	myModalForm.classList.toggle("is-open");
 }
 
 btnOpen.addEventListener("click", toggleClass);
+btnOpenMenu.addEventListener("click", toggleClass);
 btnClose.addEventListener("click", toggleClass);
